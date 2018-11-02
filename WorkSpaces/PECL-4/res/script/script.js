fdbefd336d5a83dd1f79c4c8935da6d561db4da6
D@r@s Monkey <davidariza1994@gmail.com>
2018-11-02 01:57:01 +0100

    Generado ejercicio 2.4 y 3 admin mesas y geolocalocalizacion.

--- WorkSpaces/PECL-4/res/script/script.js
+++ WorkSpaces/PECL-4/res/script/script.js
@@ -212,23 +212,23 @@ var x = document.getElementById("selecciones_platos");
 var i = x.length;
 //formateamos nuestro box de opciones
 do {
-x.remove(0);
+	x.remove(0);
     i--;
 }
 while (i > 0);
-//document.getElementById("resultado").innerHTML = i;
-var option1 = document.createElement("option");
-option1.text = "Crema Catalana";
-var option2 = document.createElement("option");
-option2.text = "Ensalada";
-var option3 = document.createElement("option");
-option3.text = "Gazpacho";
-var option4 = document.createElement("option");
-option4.text = "Parrillada";
-x.add(option1);
-x.add(option2);
-x.add(option3);
-x.add(option4);
+	//document.getElementById("resultado").innerHTML = i;
+	var option1 = document.createElement("option");
+	option1.text = "Crema Catalana";
+	var option2 = document.createElement("option");
+	option2.text = "Ensalada";
+	var option3 = document.createElement("option");
+	option3.text = "Gazpacho";
+	var option4 = document.createElement("option");
+	option4.text = "Parrillada";
+	x.add(option1);
+	x.add(option2);
+	x.add(option3);
+	x.add(option4);
 }
 
 function postres() {
@@ -239,23 +239,23 @@ var x = document.getElementById("selecciones_platos");
 var i = x.length;
 //formateamos nuestro box de opciones
 do {
-x.remove(0);
+	x.remove(0);
     i--;
 }
 while (i > 0);
-//document.getElementById("resultado").innerHTML = i;
-var option1 = document.createElement("option");
-option1.text = "Yogurt";
-var option2 = document.createElement("option");
-option2.text = "Helado";
-var option3 = document.createElement("option");
-option3.text = "Sandia";
-var option4 = document.createElement("option");
-option4.text = "Melon";
-x.add(option1);
-x.add(option2);
-x.add(option3);
-x.add(option4);
+	//document.getElementById("resultado").innerHTML = i;
+	var option1 = document.createElement("option");
+	option1.text = "Yogurt";
+	var option2 = document.createElement("option");
+	option2.text = "Helado";
+	var option3 = document.createElement("option");
+	option3.text = "Sandia";
+	var option4 = document.createElement("option");
+	option4.text = "Melon";
+	x.add(option1);
+	x.add(option2);
+	x.add(option3);
+	x.add(option4);
 }
 
 function seleccion_primero(mesas){
@@ -345,8 +345,4 @@ function myFunction(mesas){
 		pagar.value = importe ; 
 		mesas[4] = [];cambio_mesa(mesas);
 	}
-	
-	
-	
-
 }
