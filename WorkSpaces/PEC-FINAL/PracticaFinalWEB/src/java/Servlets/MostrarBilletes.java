46c0f284127b2a7771bcccbaf4efb6ae017b8063
soleab <33247822+soleab@users.noreply.github.com>
2019-01-14 18:58:40 +0100

    Mostrar billetes y añadir en la cesta

---
+++ WorkSpaces/PEC-FINAL/PracticaFinalWEB/src/java/Servlets/MostrarBilletes.java
@@ -0,0 +1,47 @@
+package Servlets;
+
+/**@author solea*/
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import utiles.ModeloDatos;
+
+public class MostrarBilletes extends HttpServlet {
+
+    private ModeloDatos bd;
+
+    public void init(ServletConfig cfg) throws ServletException {
+        bd = new ModeloDatos();
+        bd.abrirConexion();
+    }
+    
+    
+    public void destroy() {
+        bd.cerrarConexion();
+        super.destroy();
+    }
+    
+    public void service(HttpServletRequest peticion, HttpServletResponse respuesta) throws ServletException, IOException {
+
+        try {
+            HttpSession s = peticion.getSession(true);
+            //Hay que coger la fecha de ida, la fecha de vuelta, el aeropuerto origen y el aeropuerto destino
+            String f_ida=""; //coger de la pagina inicial
+            String f_vuelta="";
+            String aer_origen="";
+            String aer_destino="";
+            bd.obtenerBilletes(aer_origen,aer_destino,f_ida,f_vuelta); //pasar por parámetro lo mencionado arriba 
+            s.setAttribute("consulta", bd.getResultado());
+            respuesta.sendRedirect(respuesta.encodeRedirectURL("listado_billetes.jsp"));
+        } catch (Exception e) {
+            System.out.println("Error: "+e);
+        }
+    }
+}
+
---
+++ WorkSpaces/PEC-FINAL/PracticaFinalWEB/src/java/Servlets/ServletAnadirCesta.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+/**
+ *
+ * @author solea
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import utiles.*;
+
+public class ServletAnadirCesta extends HttpServlet {
+
+    public void service(HttpServletRequest peticion, HttpServletResponse respuesta) throws ServletException, IOException {
+
+        HttpSession session = peticion.getSession();
+        Carro carro = (Carro) session.getAttribute("carro");
+        String aer_origen = peticion.getParameter("origen");
+        String aer_destino = peticion.getParameter("dest");
+        String id = peticion.getParameter("id");
+        float precio = Float.parseFloat(peticion.getParameter("precio"));
+        String fecha = peticion.getParameter("fecha");
+        
+        carro.anadirVueloCesta(aer_origen,aer_destino,precio,fecha);
+    }
+    
+}
